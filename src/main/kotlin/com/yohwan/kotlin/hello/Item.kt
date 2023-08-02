9b78af49e48df246197cf2a791df2a7eff950683
Yohwan Lim <dyghks7102@naver.com>
2023-08-02 23:03:59 +0900

    feat. interface 사용예제 추가

--- src/main/kotlin/com/yohwan/kotlin/hello/Item.kt
+++ src/main/kotlin/com/yohwan/kotlin/hello/Item.kt
@@ -3,9 +3,25 @@ package com.yohwan.kotlin.hello
 fun main(args: Array<String>) {
     val item = Item("Book", 10000)
     println("Item name is ${item.name}, price is ${item.price}")
+
+    item.buy()
+    item.sell()
 }
 
 class Item(
     val name: String,
     val price: Int
-)
+) : ItemTrade {
+    override fun buy() {
+        println("[buy] $name")
+    }
+
+    override fun sell() {
+        println("[sell] $name")
+    }
+}
+
+interface ItemTrade {
+    fun buy()
+    fun sell()
+}
