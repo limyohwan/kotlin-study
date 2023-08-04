0b9565fd47026d6e1f93604e52b8fba930b9aae1
Yohwan Lim <dyghks7102@naver.com>
2023-08-04 23:36:00 +0900

    feat. 손수 dto validation 체크

--- src/main/kotlin/com/yohwan/kotlin/blog/service/BlogService.kt
+++ src/main/kotlin/com/yohwan/kotlin/blog/service/BlogService.kt
@@ -1,6 +1,7 @@
 package com.yohwan.kotlin.blog.service
 
 import com.yohwan.kotlin.blog.dto.BlogDto
+import com.yohwan.kotlin.core.exception.InvalidInputException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
@@ -14,6 +15,25 @@ class BlogService {
     lateinit var restApiKey: String
 
     fun searchKakao(blogDto: BlogDto): String? {
+        val msgList = mutableListOf<ExceptionMsg>()
+
+        if (blogDto.query.trim().isEmpty()) {
+            msgList.add(ExceptionMsg.EMPTY_QUERY)
+        }
+
+        if (blogDto.sort.trim() !in arrayOf("accuracy", "recency")) {
+            msgList.add(ExceptionMsg.NOT_IN_SORT)
+        }
+
+        when {
+            blogDto.page < 1 -> msgList.add(ExceptionMsg.LESS_THAN_MIN)
+            blogDto.page > 50 -> msgList.add(ExceptionMsg.MORE_THAN_MAX)
+        }
+
+        if (msgList.isNotEmpty()) {
+            val message = msgList.joinToString { it.msg }
+            throw InvalidInputException(message)
+        }
 
         val webClient = WebClient.builder()
             .baseUrl("https://dapi.kakao.com")
@@ -34,4 +54,11 @@ class BlogService {
 
         return response.block()
     }
+}
+
+private enum class ExceptionMsg(val msg: String) {
+    EMPTY_QUERY("query = no query"),
+    NOT_IN_SORT("sort = accuracy or recency"),
+    LESS_THAN_MIN("page = less than min"),
+    MORE_THAN_MAX("page = more than max")
 }
